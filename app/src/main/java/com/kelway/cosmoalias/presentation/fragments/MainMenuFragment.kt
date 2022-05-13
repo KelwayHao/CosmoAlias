576c701ee6921b62b7b27d0cfa58bf4e0d54694a
KelwayHao <kelwaycf@gmail.com>
2022-05-13 16:05:01 +0300

    Add MainMenu, SplashScreen, AboutApp

--- app/src/main/java/com/kelway/cosmoalias/presentation/fragments/MainMenuFragment.kt
+++ app/src/main/java/com/kelway/cosmoalias/presentation/fragments/MainMenuFragment.kt
@@ -1,9 +1,18 @@
 package com.kelway.cosmoalias.presentation.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.kelway.cosmoalias.R
+import com.kelway.cosmoalias.databinding.FragmentMainMenuBinding
 
 class MainMenuFragment: Fragment(R.layout.fragment_main_menu) {
+    private val binding by viewBinding<FragmentMainMenuBinding>()
 
+    override fun onStart() {
+        super.onStart()
+        binding.buttonInfo.setOnClickListener {
+            findNavController().navigate(R.id.actionMainMenuFragmentToAboutAppFragment)
+        }
+    }
 }
