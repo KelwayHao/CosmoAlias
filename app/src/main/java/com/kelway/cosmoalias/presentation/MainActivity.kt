6b1ed08792fd1d91035daa98e35922cc46a8e4ef
KelwayHao <kelwaycf@gmail.com>
2022-05-13 16:05:01 +0300

    Add MainMenu, SplashScreen, AboutApp

--- app/src/main/java/com/kelway/cosmoalias/presentation/MainActivity.kt
+++ app/src/main/java/com/kelway/cosmoalias/presentation/MainActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.kelway.cosmoalias.R
+import com.kelway.cosmoalias.databinding.ActivityMainBinding
+import com.kelway.cosmoalias.databinding.FragmentMainMenuBinding
 import com.kelway.cosmoalias.presentation.fragments.MainMenuFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding by viewBinding<ActivityMainBinding>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
