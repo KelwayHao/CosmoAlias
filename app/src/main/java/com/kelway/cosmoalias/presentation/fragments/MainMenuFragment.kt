0b2605628edcbee477e3a9b5f4f90afcbd67faba
KelwayHao <kelwaycf@gmail.com>
2022-05-13 15:33:36 +0300

    Add MainMenu, SplashScreen, AboutApp

---
+++ app/src/main/java/com/kelway/cosmoalias/presentation/fragments/MainMenuFragment.kt
@@ -0,0 +1,9 @@
+package com.kelway.cosmoalias.presentation.fragments
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.kelway.cosmoalias.R
+
+class MainMenuFragment: Fragment(R.layout.fragment_main_menu) {
+
+}
