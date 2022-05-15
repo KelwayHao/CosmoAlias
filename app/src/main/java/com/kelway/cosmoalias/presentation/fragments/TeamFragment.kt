1a65c6fd02a02941d2504800c042bc7fccf06478
KelwayHao <kelwaycf@gmail.com>
2022-05-16 00:29:17 +0300

    Add some fragment

---
+++ app/src/main/java/com/kelway/cosmoalias/presentation/fragments/TeamFragment.kt
@@ -0,0 +1,7 @@
+package com.kelway.cosmoalias.presentation.fragments
+import androidx.fragment.app.Fragment
+import com.kelway.cosmoalias.R
+
+class TeamFragment: Fragment(R.layout.fragment_team) {
+
+}
