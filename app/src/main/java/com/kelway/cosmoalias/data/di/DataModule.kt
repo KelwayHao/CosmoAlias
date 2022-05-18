0a4c63953f340ffa8786e8519ce993158497ede8
KelwayHao <kelwaycf@gmail.com>
2022-05-19 00:40:24 +0300

    add DI

---
+++ app/src/main/java/com/kelway/cosmoalias/data/di/DataModule.kt
@@ -0,0 +1,14 @@
+package com.kelway.cosmoalias.data.di
+
+import com.kelway.cosmoalias.data.repository.TeamRepositoryImpl
+import com.kelway.cosmoalias.data.room.team_room.TeamDao
+import com.kelway.cosmoalias.domain.repository.TeamRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module
+interface DataModule {
+    @Binds
+    fun bindsTeamRepository(teamRepositoryImpl: TeamRepositoryImpl): TeamRepository
+}
