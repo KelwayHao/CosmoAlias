5321c0a1189865bedbb44e242ce1ec2608e304ca
KelwayHao <kelwaycf@gmail.com>
2022-05-22 22:37:15 +0300

    new settings

--- app/src/main/java/com/kelway/cosmoalias/presentation/fragments/SettingFragment.kt
+++ app/src/main/java/com/kelway/cosmoalias/presentation/fragments/SettingFragment.kt
@@ -1,8 +1,21 @@
 package com.kelway.cosmoalias.presentation.fragments
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.kelway.cosmoalias.R
+import com.kelway.cosmoalias.databinding.FragmentSettingBinding
 
 class SettingFragment: Fragment(R.layout.fragment_setting) {
+    private val binding by viewBinding<FragmentSettingBinding>()
 
+    override fun onStart() {
+        super.onStart()
+        initView()
+    }
+
+    private fun initView() {
+        binding.buttonSaveSettings.setOnClickListener {
+
+        }
+    }
 }
