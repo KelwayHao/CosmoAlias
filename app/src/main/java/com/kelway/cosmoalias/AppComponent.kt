768a8feb2d9c7039324bd9c48848309bf1756a4c
KelwayHao <kelwaycf@gmail.com>
2022-05-24 23:13:50 +0300

    made a setting section

--- app/src/main/java/com/kelway/cosmoalias/AppComponent.kt
+++ app/src/main/java/com/kelway/cosmoalias/AppComponent.kt
@@ -5,18 +5,21 @@ import com.kelway.cosmoalias.data.di.DataModule
 import com.kelway.cosmoalias.data.di.RoomModule
 import com.kelway.cosmoalias.domain.di.DomainModule
 import com.kelway.cosmoalias.presentation.MainActivity
+import com.kelway.cosmoalias.presentation.fragments.SettingFragment
 import com.kelway.cosmoalias.presentation.teams.TeamFragment
+import com.kelway.cosmoalias.utils.di.PreferencesModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(
-    modules = [RoomModule::class, DataModule::class, DomainModule::class]
+    modules = [RoomModule::class, DataModule::class, DomainModule::class, PreferencesModule::class]
 )
 interface AppComponent {
     fun inject(target: TeamFragment)
     fun inject(target: MainActivity)
+    fun inject(target: SettingFragment)
 
     @Component.Builder
     interface Builder {
