aac980c0193856cbc4c023a7fba27c35d14b11a9
KelwayHao <kelwaycf@gmail.com>
2022-06-04 01:54:13 +0300

    Realization feature endGame

--- app/src/main/java/com/kelway/cosmoalias/utils/Constants.kt
+++ app/src/main/java/com/kelway/cosmoalias/utils/Constants.kt
@@ -7,6 +7,7 @@ class Constants {
         const val WORD_COUNT = "WORD_COUNT"
         const val ROUND_LENGTH = "ROUND_LENGTH"
         const val NUMBERS_LAPS = "NUMBERS_LAPS"
+        const val COUNT_LAPS = "COUNT_LAPS"
 
         const val REWARD_TRUE_ANSWER = 2
         const val PENALTY_FALSE_ANSWER = 1
